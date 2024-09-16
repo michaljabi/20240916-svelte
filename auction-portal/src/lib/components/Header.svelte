f975d990e574379ee716dcbe564488b20be6fe48
Jabi <michal.jabi@gmail.com>
2024-09-16 14:18:55 +0200

    explain: how to render object on Component template

--- auction-portal/src/lib/components/Header.svelte
+++ auction-portal/src/lib/components/Header.svelte
@@ -1,1 +1,23 @@
-<h1>Auction Portal</h1>
+<h1>Auction Portal {2 + 3 + 4}</h1>
+
+<div>
+    { new Date() }
+</div>
+
+<div>
+    { null }
+</div>
+
+<div>
+    { undefined }
+</div>
+
+<div>
+    { JSON.stringify({ name: 'Michał '}) }
+</div>
+
+<div>
+    <!-- co by byo jakbym chcia renderowa / interpolowa obiekt ? -->
+</div>
+
+<!-- div[title=hello]>p.article#para -->
