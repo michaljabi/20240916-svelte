bb2f4ac8ba5368ba26a36edf093fe01000187203
Jabi <michal.jabi@gmail.com>
2024-09-16 15:18:43 +0200

    feat: prepare Header code.

--- auction-portal/src/lib/components/Header.svelte
+++ auction-portal/src/lib/components/Header.svelte
@@ -1,31 +1,7 @@
-<script context="module">
-    console.log('I run only once in app lifetime...')
-</script>
-
 <script>
-    console.log('I am instance !')
-    let myNumber = 20
+
 </script>
-<h1>Auction Portal {myNumber}</h1>
 
-<div>
-    { new Date() }
-</div>
-
-<div>
-    { null }
-</div>
-
-<div>
-    { undefined }
-</div>
-
-<div>
-    { JSON.stringify({ name: 'Michał '}) }
-</div>
-
-<div>
-    <!-- co by byo jakbym chcia renderowa / interpolowa obiekt ? -->
-</div>
-
-<!-- div[title=hello]>p.article#para -->
+<header class="mb-2 p-5 bg-warning">
+    <h1>Auction portal</h1>
+</header>
--- auction-portal/src/routes/+page.svelte
+++ auction-portal/src/routes/+page.svelte
@@ -6,23 +6,23 @@
 	
 	import Header from "../lib/components/Header.svelte";
 
-	let x = 256
-	let myTitle = 'To jest tytuł, po najechaniu'
-	// debugger; //check in config (not working)
-	console.log(x);
+	// let x = 256
+	// let myTitle = 'To jest tytuł, po najechaniu'
+	// // debugger; //check in config (not working)
+	// console.log(x);
 
-	// console.log(document) later...
+	// // console.log(document) later...
 
-	setTimeout(() => {
-		x = 2;
-		myTitle = 'To jest tytuł 3'
-	}, 3000)
+	// setTimeout(() => {
+	// 	x = 2;
+	// 	myTitle = 'To jest tytuł 3'
+	// }, 3000)
 
 
 </script>
 
 <Header /> 
-<a href="/auctions" title={myTitle}>Aukcje {x}</a>
+<!-- <a href="/auctions" title={myTitle}>Aukcje {x}</a>
 
 
-<div on:click={() => x = 23333} > Mój szczęśliwy numer to: {x}</div>
+<div on:click={() => x = 23333} > Mój szczęśliwy numer to: {x}</div> -->
