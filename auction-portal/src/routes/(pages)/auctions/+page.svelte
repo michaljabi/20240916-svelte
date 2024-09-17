0fa7f1e1829a6a249c057a6ae581c76e160667a5
Jabi <michal.jabi@gmail.com>
2024-09-17 16:23:18 +0200

    feat(auctions): load data client side.

--- auction-portal/src/routes/(pages)/auctions/+page.svelte
+++ auction-portal/src/routes/(pages)/auctions/+page.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
 	import BasePage from '$lib/components/BasePage.svelte';
+	import type { AuctionItem } from '$lib/model/AuctionItem';
+	import { onMount } from 'svelte';
+	// import { getAllAuctions } from '$lib/server/auction.service';
+
+	async function callAuctions() {
+		const response = await fetch('/api/auctions');
+		const auctions = await response.json();
+		return auctions;
+	}
+
+	let isLoading = true;
+	let myAuctions: AuctionItem[] = [];
+
+	// Wrócimy do tego dlaczego to trzeba potem poprawic (w kontekscie onMount ->)
+	onMount(async () => {
+		myAuctions = await callAuctions();
+		isLoading = false;
+	});
+
+	// console.log(data);
 </script>
 
 <BasePage title="Aukcje">
@@ -9,6 +29,7 @@
 	</svelte:fragment> -->
 	<section>
 		<div class="row">
+			{JSON.stringify(myAuctions)}
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3">hello aukcja...</div>
 		</div>
 	</section>
