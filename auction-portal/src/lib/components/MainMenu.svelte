b683efe4ed83c12f5017ff89bf09a0c3a5cbcf4b
Jabi <michal.jabi@gmail.com>
2024-09-17 10:41:03 +0200

    feat(MainMenu): generate links as HTML from code.

--- auction-portal/src/lib/components/MainMenu.svelte
+++ auction-portal/src/lib/components/MainMenu.svelte
@@ -14,6 +14,38 @@
 
 	// To jaki wariant wybierzemy nie ma znaczenia (może byc tez inline). 
 	// Nie ma znaczenia z perspektywy np. testowania tego komponentu.
+
+	/*
+	interface Sample {
+		(): number
+	}
+
+	const myexample: Sample = () => 20
+	*/
+
+	// Jedna z różnic pomiędzy type a interface w TypeScript to fakt że interface jest ZAWSZE obiektowy!!!
+	// type Money = number;  
+	// type Money = { amount: number, currency: 'PLN' | 'USD' };  
+
+	/* 
+	// można też tak:
+	type MenuLink = {
+		label: string
+		link: string
+	}
+	*/
+
+	interface MenuLink {
+		label: string
+		link: string
+	}
+
+	const menuLinks: MenuLink[] = [
+		{label: 'Aukcje', link: '/auctions'},
+		{label: 'Promocje', link: '/promotions'},
+		{label: 'Podpowiadamy', link: '/advices'},
+	]
+
 </script>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 mb-3">
@@ -23,15 +55,12 @@
 	</button>
 	<div class="collapse navbar-collapse" class:show={isMenuOpen}>
 		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="/auctions">Aukcje</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="/promotions">Promocje</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="/advices">Podpowiadamy</a>
-			</li>
+			<!-- generuj poniższe za pomocą kodu:-->
+			 {#each menuLinks as item (item.label)}
+				<li class="nav-item">
+					<a class="nav-link" href={item.link}>{item.label}</a>
+				</li>
+			{/each}
 		</ul>
 	</div>
 </nav>
