89619cac57be9c688d233db33f2846e03f58d0eb
Jabi <michal.jabi@gmail.com>
2024-09-17 16:57:24 +0200

    todo: AuctionCard + render auctions

--- auction-portal/src/routes/(pages)/auctions/+page.svelte
+++ auction-portal/src/routes/(pages)/auctions/+page.svelte
@@ -30,7 +30,22 @@
 	<section>
 		<div class="row">
 			{JSON.stringify(myAuctions)}
-			<div class="col-12 col-sm-6 col-md-4 col-lg-3">hello aukcja...</div>
-		</div>
+			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+				1
+				<div class="card">
+					<div class="card-header">Tytuł</div>
+					<img class="card-img" src="https://picsum.photos/id/36/200/200" alt="" />
+					<div class="card-body">
+						<p class="card-text">Opis aukcji</p>
+						<div class="d-flex justify-content-between align-content-center">
+							<strong> 0 zł</strong>
+							<button class="btn btn-primary">
+								<i class="fa fa-cart-plus"></i>
+							</button>
+						</div>
+					</div>
+				</div>
+				<!-- <AuctionCard auction={auction} /> -->
+			</div>
 	</section>
 </BasePage>
