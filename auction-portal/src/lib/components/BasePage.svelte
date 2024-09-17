6370cc4ff160ac88b9e2873027b27644dae8063d
Jabi <michal.jabi@gmail.com>
2024-09-17 13:06:31 +0200

    feat(BasePage): use svelte:head to dynamically re-write document title.

--- auction-portal/src/lib/components/BasePage.svelte
+++ auction-portal/src/lib/components/BasePage.svelte
@@ -2,6 +2,10 @@
 	export let title: string;
 </script>
 
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
 <slot name="title">
 	<h2 class="my-4 p-2 border-bottom border-primary">{title}</h2>
 </slot>
--- auction-portal/src/routes/(pages)/promotions/+page.svelte
+++ auction-portal/src/routes/(pages)/promotions/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	import BasePage from "$lib/components/BasePage.svelte";
-
+	import BasePage from '$lib/components/BasePage.svelte';
 </script>
-<h2 class="my-4 p-2 border-bottom border-primary">Aukcje na promocji</h2>
 
 <BasePage title="Aukcje na promocji">
 	<section>...</section>
